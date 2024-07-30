15de6d949b74b52b172fec104b54859b0d93f2d7
Kirill <snamas@rambler.ru>
2024-07-30 11:51:38 +0700

    Доработка алгоритма по сбору морковки,с учетом разного количества их на каждой поляне.

--- src/Carrots/Main.java
+++ src/Carrots/Main.java
@@ -1,20 +1,65 @@
 package Carrots;
 
+import java.util.*;
+
 public class Main {
+    private final static int MOVES = 10;
+    private final static int MAX_WEIGHT_PER_MOVE = 5;
+
     public static void main(String[] args) {
-        int[] carrotsWeight = {1, 2, 3, 4, 5};
-        int moves = 10;
-        int maxWeightPerMove = 5;
-        int totalCarrotsWeight = getTotalCarrotsWeight(carrotsWeight, maxWeightPerMove, moves);
-        System.out.println("Максимальное количество моркови за 1 день: " + totalCarrotsWeight + " кг");
+        Field[] fields = {
+                new Field("Поле 1", 8, 1),
+                new Field("Поле 2", 5, 2),
+                new Field("Поле 3", 5, 3),
+                new Field("Поле 4", 4, 4),
+                new Field("Поле 5", 5, 5)
+        };
+
+        int totalCarrotsWeight = calculateTotalCarrotsWeight(fields);
+
+        System.out.println("Максимальное количество моркови за 10 попыток: " + totalCarrotsWeight + " кг");
     }
-    private static int getTotalCarrotsWeight(int[] carrotWeights, int maxWeightPerMove, int moves) {
-        int max = Integer.MIN_VALUE;
-        for (int carrotWeight : carrotWeights) {
-            if (max <= carrotWeight && carrotWeight <= maxWeightPerMove) {
-                max = carrotWeight;
+
+    private static int calculateTotalCarrotsWeight(Field[] fields) {
+        List<Integer> weightMoveList = new ArrayList<>();
+        for (Field field : fields) {
+            int weightCarrotOnField = field.getCarrotsWeight();
+            int availableCarrotsOnField = field.getCarrotsCount();
+            while (availableCarrotsOnField > 0) {
+                if (weightCarrotOnField > MAX_WEIGHT_PER_MOVE) {
+                    break;
+                }
+                int maxCarrotsToTake = MAX_WEIGHT_PER_MOVE / weightCarrotOnField;
+                int carrotsToTake = Math.min(maxCarrotsToTake, availableCarrotsOnField);
+                availableCarrotsOnField -= carrotsToTake;
+                weightMoveList.add(carrotsToTake * weightCarrotOnField);
             }
         }
-        return max*moves;
+        return weightMoveList.stream().sorted(Comparator.reverseOrder())
+                .limit(MOVES).mapToInt(Integer::intValue).sum();
+    }
+}
+
+class Field {
+    private String name;
+    private int carrotsCount;
+    private int carrotsWeight;
+
+    public Field(String name, int carrotsCount, int carrotsWeight) {
+        this.name = name;
+        this.carrotsCount = carrotsCount;
+        this.carrotsWeight = carrotsWeight;
+    }
+
+    public int getCarrotsCount() {
+        return carrotsCount;
+    }
+
+    public int getCarrotsWeight() {
+        return carrotsWeight;
+    }
+
+    public String getName() {
+        return name;
     }
 }
